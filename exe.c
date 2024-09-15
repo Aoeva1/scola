56cd627ce5e97b60765d074a5d167664cc5298cc
aoeva <calakjohn187@gmail.com>
2024-09-14 20:07:31 -0400

    first commit

---
+++ exe.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // ASCII Art for Sans from Undertale
+    const char *sans_art = 
+        "  _____ \n"
+        " /     \\ \n"
+        "| () () |\n"
+        "|  . .  |\n"
+        " \\_____/\n"
+        "   | |\n"
+        "  /   \\\n";
+
+    printf("%s", sans_art);
+
+    return 0;
+}
